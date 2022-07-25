8b1a7b5422cba6dcb9b4d61192c73faea23020fe
haythemch2 <haythem.chibani@gmail.com>
2022-07-25 14:39:36 +0100

    meta

--- components/layout.tsx
+++ components/layout.tsx
@@ -24,10 +24,7 @@ const layout = ({ children }: Props) => {
           property="og:description"
           content="USDT WHALE TRANSFERS TRACKER WITH A THRESHOLD OF 1M$"
         />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="og:image" content="/meta.jpg" />
 
         <link rel="icon" href="/g.png" />
       </Head>
